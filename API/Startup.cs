fb7883e6a736b503fb14957599bcc13fdeea6723
Maurizio <mrz.strazzullo@gmail.com>
2020-10-20 00:37:39 +0200

    Add Angular Client View

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,7 @@ namespace API {
                 options.UseSqlServer (_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers ();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +39,9 @@ namespace API {
 
             app.UseHttpsRedirection ();
 
-            app.UseRouting ();
+            app.UseRouting();
+
+            app.UseCors( x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization ();
 
